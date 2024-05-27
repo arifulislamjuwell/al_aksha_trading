2ccda5bddc7bb1b1b2623c0f1f8f960d0e4c52a7
Ariful Islam Juwel <juwelariful1@gmail.com>
2024-05-27 22:48:20 +0600

    setting updated

--- al_aksha_trading/settings.py
+++ al_aksha_trading/settings.py
@@ -110,26 +110,26 @@ if config('LOCAL', cast=bool):
     STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-    DATABASES = {
+
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#             'NAME': config('DB_NAME'),
+#             'USER': config('DB_USER'),
+#             'PASSWORD': config('DB_PASSWORD'),
+#             'HOST': 'localhost',
+#             'PORT': '5432',
+#         }
+# }
+
+DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': str(BASE_DIR / "db.sqlite3"),
         }
     }
-else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': config('DB_NAME'),
-            'USER': config('DB_USER'),
-            'PASSWORD': config('DB_PASSWORD'),
-            'HOST': 'localhost',
-            'PORT': '5432',
-        }
-}
-
-
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media') 
 MEDIA_URL = '/media/'
 
